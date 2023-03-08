c995c77a85190e2d590314120ff9cd5a50455a64
sowjanyamerugu <125579978+sowjanyamerugu@users.noreply.github.com>
2023-03-08 17:57:13 -0500

    Add files via upload

---
+++ Entry_wig.py
@@ -0,0 +1,10 @@
+# creating a simple tkinter window
+# if you are using python2
+# use import Tkinter as tk
+
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("First Application")
+root.mainloop()
